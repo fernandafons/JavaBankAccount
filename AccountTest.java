2bbf46fff0af2990d5406880b9262e828389dbb9
fernandafons <nanda.fonseca7@gmail.com>
2019-10-21 10:24:31 -0300

    Initial commit

---
+++ AccountTest.java
@@ -0,0 +1,40 @@
+class AccountTest {
+	public static void main(String[] args) {
+		Account Account1 = new Account();
+		Account1.setOwner("Maria");
+		Account1.setBalance(25000.00);
+		Account1.setNumber("757698-2");
+		Account1.setbankAgency("5200");
+		Account1.setdateOfActivation("20/02/2000");
+		
+		
+		System.out.println("Nome do cliente: " + Account1.getOwner());
+		System.out.println("Número da conta do cliente: " + Account1.getNumber());
+		System.out.println("Agência bancária: " + Account1.getbankAgency());
+		System.out.println("Saldo atual: " + Account1.getBalance());
+		
+		// saca 200 reais
+		Account1.withdraw(200);
+		System.out.println("Saldo depois do saque de 200 reais: " + Account1.getBalance());
+		
+		// deposita 500 reais
+		Account1.deposit(500);
+		System.out.println("Saldo depois do depósito de 500 reais: " + Account1.getBalance());
+		
+		// printa rendimento
+		Account1.calcYield();
+		System.out.println("Rendimento atual: " + Account1.getYield());
+		
+		
+		Account Account2 = new Account();
+		Account1.setOwner("João");
+		Account1.setBalance(10000.00);
+		Account1.setNumber("758998-x");
+		Account1.setbankAgency("5260");
+		Account1.setdateOfActivation("19/04/2001");
+		
+		System.out.println("Realizando transfência " + Account1.transferTo(Account2, 50));
+		System.out.println("Atual saldo da Maria: " + Account1.getBalance());
+		System.out.println("Atual saldo do João: " + Account2.getBalance());
+	}
+}
